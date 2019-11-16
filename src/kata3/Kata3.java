a8925f06c985af6c8354c2d48aa6e0c9828ea7d2
Familia <Familia@DESKTOP-U4D87FT>
2019-11-16 14:14:55 +0000

    First Draft, check database errors

---
+++ src/kata3/Kata3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kata3;
+
+import java.util.List;
+import kata3.view.HistogramDisplay;
+import kata3.model.Histogram;
+public class Kata3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        HistogramDisplay display = new HistogramDisplay();//O
+        display.execute(); //O
+    }
+    
+}
